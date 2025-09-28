ddf70e59e33cf0aec594ba4e71db8cdc995f5b2f
Sebastian337 <aleshka_demidov@mail.ru>
2025-09-28 13:54:21 +0300

    Update vk_example.py

--- vk_example.py
+++ vk_example.py
@@ -3,74 +3,67 @@ from urllib.parse import urlparse
 from decouple import config
 
 
-TOKEN = config('VK_TOKEN')
+TOKEN = config("VK_TOKEN")
 
 
 def shorten_link(token, url):
-    api_url = 'https://api.vk.com/method/utils.getShortLink'
+    api_url = "https://api.vk.com/method/utils.getShortLink"
     params = {
-        'access_token': token,
-        'url': url,
-        'v': '5.199'
+        "access_token": token,
+        "url": url,
+        "v": "5.199",
     }
     response = requests.get(api_url, params=params)
-    data = response.json()
+    response.raise_for_status()
 
-    if 'error' in data:
-        return None
+    response_json = response.json()
 
-    return data['response']['short_url']
+    if "error" in response_json:
+        raise requests.exceptions.HTTPError(response_json["error"])
+
+    return response_json["response"]["short_url"]
 
 
 def count_clicks(token, short_url):
-    key = urlparse(short_url).path.lstrip('/')
-    api_url = 'https://api.vk.com/method/utils.getLinkStats'
+    key = urlparse(short_url).path.lstrip("/")
+    api_url = "https://api.vk.com/method/utils.getLinkStats"
     params = {
-        'access_token': token,
-        'key': key,
-        'interval': 'forever',
-        'v': '5.199'
+        "access_token": token,
+        "key": key,
+        "interval": "forever",
+        "v": "5.199",
     }
     response = requests.get(api_url, params=params)
-    data = response.json()
+    response.raise_for_status()
 
-    if 'error' in data:
-        return None
+    stats_json = response.json()
+    if "error" in stats_json:
+        raise requests.exceptions.HTTPError(stats_json["error"])
 
-    stats = data['response']['stats']
-    return sum(item['views'] for item in stats)
+    stats = stats_json["response"]["stats"]
+    return sum(item["views"] for item in stats)
 
 
-def is_shorten_link(token, url):
-    key = urlparse(url).path.lstrip('/')
-    api_url = 'https://api.vk.com/method/utils.getLinkStats'
-    params = {
-        'access_token': token,
-        'key': key,
-        'interval': 'day',
-        'v': '5.199'
-    }
-    response = requests.get(api_url, params=params)
-    data = response.json()
-    return 'response' in data
+def is_shorten_link(url):
+
+    return "vk.cc" in url
 
 
 def main():
-    user_url = input('Введите ссылку: ')
+    user_url = input("Введите ссылку: ")
 
-    if is_shorten_link(TOKEN, user_url):
-        clicks = count_clicks(TOKEN, user_url)
-        if clicks is not None:
-            print('Всего переходов по ссылке:', clicks)
+    try:
+        if is_shorten_link(user_url):
+            clicks = count_clicks(TOKEN, user_url)
+            print("Всего переходов по ссылке:", clicks)
         else:
-            print('Ошибка: не удалось получить статистику.')
-    else:
-        short = shorten_link(TOKEN, user_url)
-        if short:
-            print('Сокращенная ссылка:', short)
-        else:
-            print('Ошибка: неправильная ссылка.')
+            short_link = shorten_link(TOKEN, user_url)
+            print("Сокращенная ссылка:", short_link)
+    except requests.exceptions.RequestException as e:
+        print("Некорректная ссылка или ошибка HTTP:", e)
+    except requests.exceptions.HTTPError as e:
+        print("Ошибка VK API:", e)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
