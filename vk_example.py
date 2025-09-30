a3e7185b530ea2a88ca89d85120507d91a429932
Sebastian337 <aleshka_demidov@mail.ru>
2025-09-30 14:54:16 +0300

    Update vk_example.py

--- vk_example.py
+++ vk_example.py
@@ -3,9 +3,6 @@ from urllib.parse import urlparse
 from decouple import config
 
 
-TOKEN = config("VK_TOKEN")
-
-
 def shorten_link(token, url):
     api_url = "https://api.vk.com/method/utils.getShortLink"
     params = {
@@ -16,12 +13,12 @@ def shorten_link(token, url):
     response = requests.get(api_url, params=params)
     response.raise_for_status()
 
-    response_json = response.json()
+    vk_response = response.json()
 
-    if "error" in response_json:
-        raise requests.exceptions.HTTPError(response_json["error"])
+    if "error" in vk_response:
+        raise requests.exceptions.HTTPError(vk_response["error"])
 
-    return response_json["response"]["short_url"]
+    return vk_response["response"]["short_url"]
 
 
 def count_clicks(token, short_url):
@@ -36,28 +33,40 @@ def count_clicks(token, short_url):
     response = requests.get(api_url, params=params)
     response.raise_for_status()
 
-    stats_json = response.json()
-    if "error" in stats_json:
-        raise requests.exceptions.HTTPError(stats_json["error"])
+    vk_response = response.json()
+    if "error" in vk_response:
+        raise requests.exceptions.HTTPError(vk_response["error"])
 
-    stats = stats_json["response"]["stats"]
+    stats = vk_response["response"]["stats"]
     return sum(item["views"] for item in stats)
 
 
-def is_shorten_link(url):
+def is_shorten_link(token, url):
+    key = urlparse(url).path.lstrip("/")
+    api_url = "https://api.vk.com/method/utils.getLinkStats"
+    params = {
+        "access_token": token,
+        "key": key,
+        "interval": "day",
+        "v": "5.199",
+    }
+    response = requests.get(api_url, params=params)
+    response.raise_for_status()
 
-    return "vk.cc" in url
+    vk_response = response.json()
+    return "response" in vk_response
 
 
 def main():
+    token = config("VK_TOKEN")
     user_url = input("Введите ссылку: ")
 
     try:
-        if is_shorten_link(user_url):
-            clicks = count_clicks(TOKEN, user_url)
+        if is_shorten_link(token, user_url):
+            clicks = count_clicks(token, user_url)
             print("Всего переходов по ссылке:", clicks)
         else:
-            short_link = shorten_link(TOKEN, user_url)
+            short_link = shorten_link(token, user_url)
             print("Сокращенная ссылка:", short_link)
     except requests.exceptions.RequestException as e:
         print("Некорректная ссылка или ошибка HTTP:", e)
